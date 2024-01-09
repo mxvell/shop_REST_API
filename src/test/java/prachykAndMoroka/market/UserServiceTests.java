b8c2a6c8e361614823e4e111ab2df54d70267473
maxim <11maxwellwork11@gmail.com>
2024-01-09 15:48:59 +0200

    fixed all tests userService

--- src/test/java/prachykAndMoroka/market/UserServiceTests.java
+++ src/test/java/prachykAndMoroka/market/UserServiceTests.java
@@ -6,15 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Component;
 import prachykAndMoroka.market.model.Order;
 import prachykAndMoroka.market.model.User;
 import prachykAndMoroka.market.service.UserService;
 
-import java.util.ArrayList;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -48,63 +46,41 @@ class UserServiceTests {
         assertEquals(testUserInDatabase, myUser);
     }
 
-//    @Test
-//    void testFindByIdWhenUserDoesNotExist() {
-////        int userId = 2;
-////        Mockito.when(userRepository.findById(anyInt())).thenReturn(Optional.empty());
-////        User result = userService.findById(userId);
-//        assertNull(result);
-//    }
-//
-//    @Test
-//    void testFindByNameWhenUsersExist() {
-////        String name = "John Doe";
-////        List<User> mockUsers = Arrays.asList(new User(1, name), new User(2, name));
-////        Mockito.when(userRepository.findByName(anyString())).thenReturn(mockUsers);
-////
-////
-////        List<User> result = userService.findByName(name);
-//
-//
-//        assertEquals(mockUsers, result);
-//    }
-//
-//    @Test
-//    void testFindByNameWhenNoUsersExist() {
-//
-////        String name = "Nonexistent Name";
-////        Mockito.when(userRepository.findByName(anyString())).thenReturn(Arrays.asList());
-////
-////        List<User> result = userService.findByName(name);
-//
-//        assertEquals(0, result.size());
-//    }
-//
-//    @Test
-//    void testFindByEmailWhenUserExists() {
-////        String email = "weqeqw@gmail.com";
-////        User user = new User(1, "Rock");
-////        Mockito.when(userRepository.findByEmail(anyString())).thenReturn(user);
-////        User result = userService.findByEmail(email);
-//        assertEquals(testUserInDatabase, result);
-//    }
-//
-//    @Test
-//    void testFindByEmailWhenNoUserExists() {
-//
-////        String nonExistentEmail = "nonexistent@gmail.com";
-////        Mockito.when(userRepository.findByEmail(anyString())).thenReturn(null);
-////
-////        User result = userService.findByEmail(nonExistentEmail);
-//
-//        assertNull(result);
-//    }
-//
-//    @Test
-//    void testSaveUser() {
-////        User user = new User(1, "John", "Doe", "213213@gmasil.com");
-////        userService.saveUser(user);
-////        Mockito.verify(userRepository, Mockito.times(1)).save(any(User.class));
-//    }
+    @Test
+    void testFindByIdWhenUserDoesNotExist() {
+      User myUser = userService.findById(2);
+      assertNotEquals(testUserInDatabase,myUser,"This user doesn't exist with this id");
+    }
+
+    @Test
+    void testFindByNameWhenUsersExist() {
+      List<User> myUser = userService.findByName("John");
+        //TODO
+      assertEquals(testUserInDatabase,myUser);
+
+    }
+
+    @Test
+    void testFindByNameWhenNoUsersExist() {
+
+
+        List<User> myUser = userService.findByName("AQAQAQ");
+
+        assertNotEquals(testUserInDatabase,myUser, "That user didn't find by name because it doesn't have one");
+    }
+
+    @Test
+    void testFindByEmailWhenUserExists() {
+      User emailUser = userService.findByEmail("213213@gmasil.com");
+        assertEquals(testUserInDatabase,  emailUser);
+    }
+
+    @Test
+    void testFindByEmailWhenNoUserExists() {
+        User emailUser = userService.findByEmail("trterte.@gmail.com");
+        assertNotEquals(testUserInDatabase,emailUser, "Doesn't exist email");
+
+    }
+
 }
 
